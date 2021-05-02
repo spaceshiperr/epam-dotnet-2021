b470f14a219f8e4473c4d1eaba6dd3afc7ce4054
spaceshiperr <umfd@protonmail.com>
2021-05-02 16:39:49 +0300

    Complete Task2 of M02

---
+++ M02_FaizullinaD/Perfomance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Perfomance
+{
+    class Program
+    {
+        public const int Count = 100000;
+
+        public class C
+        {
+            public int i;
+        }
+
+        public struct S
+        {
+            public int i;
+        }
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var process = Process.GetCurrentProcess();
+            
+            Console.WriteLine("PrivateMemorySize64 before initializing an array of classes: " + process.PrivateMemorySize64);
+
+            C[] classArray = new C[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                classArray[i] = new C() { i = random.Next() };
+            }
+
+            process.Refresh();
+            Console.WriteLine("PrivateMemorySize64 after initializing an array of classes: " + process.PrivateMemorySize64);
+            Console.WriteLine("PrivateMemorySize64 before initializing an array of structs: " + process.PrivateMemorySize64);
+
+            S[] structArray = new S[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                structArray[i].i = random.Next();
+            }
+
+            process.Refresh();
+            Console.WriteLine("PrivateMemorySize64 after initializing an array of structs: " + process.PrivateMemorySize64);
+        }
+    }
+}
