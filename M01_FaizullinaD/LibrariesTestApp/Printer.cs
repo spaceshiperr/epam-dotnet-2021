cbf5b56797835b6f674c1d1b080514c697b64591
spaceshiperr <umfd@protonmail.com>
2021-05-02 21:35:52 +0300

    Add more structure to the project

---
+++ M01_FaizullinaD/LibrariesTestApp/Printer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LibrariesTestApp
+{
+    /// <summary>
+    /// Prints the information to the console
+    /// </summary>
+    class Printer
+    {
+        /// <summary>
+        /// Prints the elements of the array into a line separated by spaces
+        /// </summary>
+        /// <param name="array">An input array</param>
+        public static void PrintArray(decimal[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Prints the list of commands for the LibrariesConsoleApp
+        /// </summary>
+        public static void PrintCommands()
+        {
+            Console.WriteLine("Choose a command:" + Environment.NewLine +
+                              "1 - bubble sort an array," + Environment.NewLine +
+                              "2 - sum all positive numbers of a two dimensional array," + Environment.NewLine +
+                              "3 - calculate perimeter or square of a rectangle," + Environment.NewLine +
+                              "0 - exit an application");
+        }
+
+        /// <summary>
+        /// Prints the message
+        /// </summary>
+        /// <param name="message">The message to print</param>
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- M01_FaizullinaD/LibrariesTestApp/Program.cs
+++ M01_FaizullinaD/LibrariesTestApp/Program.cs
@@ -11,15 +11,13 @@ namespace LibrariesTestApp
         public static void Main()
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
 
-            var action = string.Empty;
-
-            while (action != "0")
+            string command;
+            do
             {
-                PrintInfo();
-                action = Console.ReadLine();
-                switch (action)
+                Printer.PrintCommands();
+                command = Console.ReadLine();
+                switch (command)
                 {
                     case "0":
                         break;
@@ -33,177 +31,98 @@ namespace LibrariesTestApp
                         CalculateRectangle();
                         break;
                     default:
-                        Console.WriteLine("Wrong action number, try again!");
+                        Console.WriteLine("Wrong action number. Try again!");
                         break;
                 }
-            }
-        }
-
-        private static void PrintInfo()
-        {
-            Console.WriteLine("Choose an action:" + Environment.NewLine +
-                              "1 - bubble sort an array," + Environment.NewLine +
-                              "2 - sum all positive numbers of a two dimensional array," + Environment.NewLine +
-                              "3 - calculate perimeter or square of a rectangle," + Environment.NewLine +
-                              "0 - exit an application");
-        }
-
-        private static void PrintArray(decimal[] array)
-        {
-            foreach (var element in array)
-                Console.Write(element + " ");
-            Console.WriteLine();
-        }
-
-        private static decimal[] GetArray(string line)
-        {
-            if (line.Equals(string.Empty))
-                throw new ArgumentException("The entry should not be empty. Try again!");
-
-            string[] elements = line.Split(' ');
-            var array = new decimal[elements.Length];
-            
-            for (int i = 0; i < elements.Length; i++)
-            {
-                try
-                {
-                    array[i] = GetNumber(elements[i]);
-                }
-                catch (Exception e)
-                {
-                    throw e;
-                }
-            }
-            return array;
-        }
-
-        private static decimal GetNumber(string stringNumber)
-        {
-            decimal numericValue;
-            bool isNumber = decimal.TryParse(stringNumber, out numericValue);
-            if (isNumber)
-                return numericValue;
-            else
-                throw new ArgumentException("It must be a number or numbers. Try again!");
+            } while (command != "0");
         }
 
         private static void BubbleSort()
         {
-            while (true)
+            do
             {
-                Console.WriteLine("Enter the array - numbers separated by spaces:");
-                var line = Console.ReadLine();
-                decimal[] array;
                 try
                 {
-                    array = GetArray(line);
+                    Printer.Print("Enter the array - numbers separated by spaces:");
+
+                    var inputArray = Reader.GetInputArray();
+                    decimal[] array = Converter.ToNumberArray(inputArray);
+
+                    var sorter = new ArraySorter(array);
+
+                    sorter.BubbleSortAsc();
+                    Printer.Print("The array sorted in ascending order:");
+                    Printer.PrintArray(array);
+
+                    sorter.BubbleSortDesc();
+                    Printer.Print("The array sorted in descending order:");
+                    Printer.PrintArray(array);
+                    break;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Printer.Print(e.Message);
                     continue;
                 }
-
-                var sorter = new ArraySorter(array);
-
-                sorter.BubbleSortAsc();
-                Console.WriteLine("The array sorted in ascending order:");
-                PrintArray(array);
-
-                sorter.BubbleSortDesc();
-                Console.WriteLine("The array sorted in descending order:");
-                PrintArray(array);
-                break;
-            }
+            } while (true);
         }
 
         private static void SumPositives()
         {
-            while (true)
+            do
             {
-                Console.WriteLine("Enter the number of rows in a two-dimensional array");
-                string line = Console.ReadLine();
-                int n;
-                int m = 0;
-                decimal[][] array;
-
                 try
                 {
-                    n = Convert.ToInt32(GetNumber(line));
+                    Printer.Print("Enter the number of rows in a two-dimensional array:");
+                    var rowsInput = Reader.GetInput();
+                    var rows = Convert.ToInt32(Converter.ToNumber(rowsInput));
 
-                    array = new decimal[n][];
+                    Printer.Print("Enter members of the a two-dimensional array - rows of numbers separated by spaces:");
+                    string[] inputArray = Reader.GetInputArray(rows);
+                    decimal[,] array = Converter.To2DNumberArray(inputArray);
+
+                    var summator = new PositivesSummator(array);
+                    var sum = summator.SumPositives();
+                    Printer.Print("The sum of all positive numbers of the array: " + sum);
+                    break;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Printer.Print(e.Message);
                     continue;
                 }
-
-                Console.WriteLine("Enter members of the a two-dimensional array - rows of numbers separated by spaces:");
-
-                for (int i = 0; i < n; i++)
-                {
-                    line = Console.ReadLine();
-
-                    try
-                    {
-                        array[i] = GetArray(line);
-                        if (i == 0)
-                            m = array[0].Length;
-                        else if (array[i].Length != m)
-                            throw new ArgumentException("The number of elements in a row must be the same. Try again!");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        continue;
-                    }
-                }
-
-                var summator = new PositivesSummator(array);
-                var sum = summator.SumPositives();
-                Console.WriteLine("The sum of all positive numbers of the array: " + sum);
-                break;
-            }
+            } while (true);
         }
 
         private static void CalculateRectangle()
         {
-            Console.WriteLine("Enter width and length of a rectangle - two numbers separated by spaces:");
-            
-            while (true)
+            do
             {
-                var line = Console.ReadLine();
-                decimal width, length;
-
                 try
                 {
-                    var sizes = GetArray(line);
+                    Printer.Print("Enter width and length of a rectangle - two numbers separated by spaces:");
+                    var inputSizes = Reader.GetInputArray();
+                    var sizes = Converter.ToNumberArray(inputSizes);
 
                     if (sizes.Length != 2)
-                        throw new ArgumentException("There must be two number - length and width. Try again!");
+                        throw new ArgumentException("There must be two numbers - length and width. Try again!");
 
-                    width = sizes[0];
-                    length = sizes[1];
+                    var width = sizes[0];
+                    var length = sizes[1];
 
-                    if (width <= 0 | length <= 0)
-                    {
-                        throw new ArgumentException("Width and length must be positive numbers. Try again!");
-                    }
+                    var calculator = new RectangleCalculator(width, length);
+                    var perimeter = calculator.GetPerimeter();
+                    var square = calculator.GetSquare();
+
+                    Printer.Print("The perimeter is " + perimeter + " and the square is " + square);
+                    break;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Printer.Print(e.Message);
                     continue;
                 }
-
-                var calculator = new RectangleCalculator(width, length);
-                var perimeter = calculator.GetPerimeter();
-                var square = calculator.GetSquare();
-
-                Console.WriteLine("The perimeter is " + perimeter + " and the square is " + square);
-                break;
-            }
+            } while (true);
         }
     }
 }
