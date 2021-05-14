b9ae6220ee725a738242a0b0b6357098e2bb5674
spaceshiperr <umfd@protonmail.com>
2021-05-14 22:50:17 +0300

    Finished M05

---
+++ M05_FaizullinaD/ConverterApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using ConverterLibrary;
+
+namespace ConverterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging((builder) => builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace));
+            var provider = services.BuildServiceProvider();
+
+            var factory = provider.GetService<ILoggerFactory>();
+            factory.AddNLog();
+            factory.ConfigureNLog("NLog.config");
+
+            var logger = provider.GetService<ILogger<Program>>();
+
+            try
+            {
+                var converter = new ToIntConverter(logger);
+
+                Console.WriteLine("Input string:");
+                string input = Console.ReadLine();
+                int num = converter.ToInt(input);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ M05_FaizullinaD/ConverterLibrary/ToIntConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace ConverterLibrary
+{
+    public class ToIntConverter
+    {
+        public ToIntConverter(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        private static ILogger Logger;
+
+        public int ToInt(string str)
+        {
+            checkNullOrEmpty(str);
+            Logger.LogTrace("Input string received");
+
+            int num = 0;
+            Logger.LogTrace("Variable for int value initialized with 0");
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int digit = ((int)str[i] - 48);
+                checkDigit(digit);
+                Logger.LogTrace("Next digit is " + digit);
+
+                checkRange(num, digit);
+                num = num * 10 + digit;
+                Logger.LogTrace("The processed part of the number equals to " + num);
+            }
+
+            Logger.LogTrace("Final number equals to " + num);
+            return num;
+        }
+
+        private void checkNullOrEmpty(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                var ex = new ArgumentNullException("Cannot convert to int, the argument is null or empty");
+                Logger.LogError(ex.Message);
+                throw ex;
+            }
+        }
+
+        private void checkDigit(int digit)
+        {
+            if (digit > 9 || digit < 0)
+            {
+                var ex = new ArgumentException("Cannot convert to int, it contains non-digit symbols");
+                Logger.LogError(ex.Message);
+                throw ex;
+            }
+        }
+
+        private void checkRange(int num, int digit)
+        {
+            try
+            {
+                num = checked(num * 10 + digit);
+            }
+            catch (OverflowException)
+            {
+                var ex = new ArgumentOutOfRangeException("Cannot convert to int, value is out of int range");
+                Logger.LogError(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
