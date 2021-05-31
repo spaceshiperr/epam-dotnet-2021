bfad30f6428dc4e91f20cae470d37b85c194d172
spaceshiperr <umfd@protonmail.com>
2021-05-31 21:55:46 +0300

    Finish task2 of M08

---
+++ M08_FaizullinaD/GenericsLibrary.Tests/FibonacciNumbersTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using static GenericsLibrary.FibonacciNumbers;
+using System.Numerics;
+
+namespace GenericsLibrary.Tests
+{
+    [TestFixture]
+    public class FibonacciNumbersTests
+    {
+        [Test]
+        public void GetNumbers_TakeFirst10_ReturnsIEnumerableOfFirst10()
+        {
+            BigInteger[] expected = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+            var result = GetNumbers().Take(10).ToArray();
+
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ M08_FaizullinaD/GenericsLibrary/FibonacciNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace GenericsLibrary
+{
+    public static class FibonacciNumbers
+    {
+        public static IEnumerable<BigInteger> GetNumbers()
+        {
+            BigInteger previous = 0;
+            BigInteger current = 1;
+
+            while(true)
+            {
+                yield return current;
+
+                var next = previous + current;
+                previous = current;
+                current = next;
+            }
+        }
+    }
+}
